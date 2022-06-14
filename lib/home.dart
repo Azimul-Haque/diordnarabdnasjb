d1933d67c13f3d5c7a01b6123e824ee33e3c8300
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-15 04:00:04 +0600

    Commit Wed 06/15/2022  4:00:04.20 11565

--- lib/home.dart
+++ lib/home.dart
@@ -23,6 +23,7 @@ class _HomeState extends State<Home> {
     super.initState();
     uid = FirebaseAuth.instance.currentUser!.uid;
     userdata = FirebaseAuth.instance.currentUser!;
+    // ignore: avoid_print
     print(userdata);
   }
 
