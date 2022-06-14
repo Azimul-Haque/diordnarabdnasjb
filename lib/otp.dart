d248e105c077aaeb6b7c4b98d611b3d3fa9280f0
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-15 02:21:01 +0600

    Commit Wed 06/15/2022  2:21:01.20 10236

--- lib/otp.dart
+++ lib/otp.dart
@@ -125,6 +125,7 @@ class _OTPScreenState extends State<OTPScreen> {
                           verificationId: _verificationCode, smsCode: pin))
                       .then((value) async {
                     if (value.user != null) {
+                      print(value.user!.uid);
                       showSimpleSnackBar(
                           context, 'সফলভাবে লগইন সম্পন্ন হয়েছে!');
                       Navigator.pushAndRemoveUntil(
