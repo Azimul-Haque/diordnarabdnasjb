7941130be4e07a595a1016c7db24e5c67abdda07
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-15 22:34:30 +0600

    Commit Wed 06/15/2022 22:34:30.15 23539

--- lib/home.dart
+++ lib/home.dart
@@ -278,4 +278,36 @@ class _HomeState extends State<Home> {
       // print(_);
     }
   }
+
+  _postAddUser(user) async {
+    var data = {
+      'uid': user.uid,
+      'name': user.displayName ?? 'No Name',
+      'mobile': user.phoneNumber,
+      'softtoken': 'Rifat.Admin.2022',
+    };
+    // print(data);
+    try {
+      http.Response response = await http.post(
+        Uri.parse('http://192.168.0.108:8000/api/adduser'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=utf-8',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode(data),
+      );
+      // print(response.statusCode);
+      if (response.statusCode == 200) {
+        var body = json.decode(response.body);
+        if (body["success"] == true) {
+          print(body);
+        }
+      } else {
+        print(response.body);
+      }
+    } catch (_) {
+      // catch e kichu korini ekhono...
+      print(_);
+    }
+  }
 }
