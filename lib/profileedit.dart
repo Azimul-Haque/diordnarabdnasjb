f04896d3c198a96620d05909a8e6e3d6fef806f5
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-16 02:15:23 +0600

    Commit Thu 06/16/2022  2:15:23.19 31517

--- lib/profileedit.dart
+++ lib/profileedit.dart
@@ -33,9 +33,38 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
         children: [
           Container(
             margin: EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10),
-            child: Text(
-              'নামঃ',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false otherwise.
+                        if (_formKey.currentState!.validate()) {
+                          // If the form is valid, display a snackbar. In the real world,
+                          // you'd often call a server or save the information in a database.
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Processing Data')),
+                          );
+                        }
+                      },
+                      child: const Text('Submit'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
