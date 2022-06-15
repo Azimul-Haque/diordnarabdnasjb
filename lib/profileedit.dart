868010db71b50cbf9ec4df708e2967d990d369d0
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-16 02:37:14 +0600

    Commit Thu 06/16/2022  2:37:14.15 29852

--- lib/profileedit.dart
+++ lib/profileedit.dart
@@ -88,8 +88,6 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
                     child: ElevatedButton(
                       onPressed: () {
                         if (_formKey.currentState!.validate()) {
-                          // If the form is valid, display a snackbar. In the real world,
-                          // you'd often call a server or save the information in a database.
                           ScaffoldMessenger.of(context).showSnackBar(
                             const SnackBar(content: Text('Processing Data')),
                           );
