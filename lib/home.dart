99213227f7ff3e9b331197a475c44121ec130056
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-16 01:49:16 +0600

    Commit Thu 06/16/2022  1:49:16.17 5047

--- lib/home.dart
+++ lib/home.dart
@@ -112,7 +112,7 @@ class _HomeState extends State<Home> {
                 ),
                 onPressed: () {
                   Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => ProfileEditPage(userdata)));
+                      builder: (context) => ProfileEditPage(this.userdata)));
                 },
               ),
             ),
