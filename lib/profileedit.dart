afa9d9822c63034322005fe3c511db94cef36966
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-16 02:28:27 +0600

    Commit Thu 06/16/2022  2:28:27.19 26519

--- lib/profileedit.dart
+++ lib/profileedit.dart
@@ -14,7 +14,7 @@ class ProfileEditPage extends StatefulWidget {
 class _ProfileEditPageState extends State<ProfileEditPage> {
   final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
   final _formKey = GlobalKey<FormState>();
-  late final String _nameonform;
+  late String _nameonform;
 
   @override
   void initState() {
