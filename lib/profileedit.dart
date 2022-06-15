2ed3e3fe02204a1d6d8adff95f5be6ac2837c4e3
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-16 01:20:04 +0600

    Commit Thu 06/16/2022  1:20:04.16 10263

--- lib/profileedit.dart
+++ lib/profileedit.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors, prefer_const_constructors, unused_local_variable, avoid_print, unused_field
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:bjsandbarexam/globals.dart';
 
 class ProfileEditPage extends StatefulWidget {
+  ProfileEditPage(User userdata);
+
   // final String phone;
   // ProfileEditPage(this.phone);
   @override
