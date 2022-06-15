356fc4490a7bcdac573380b77b6106cb04af4cc7
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-16 02:15:36 +0600

    Commit Thu 06/16/2022  2:15:36.22 10715

--- lib/profileedit.dart
+++ lib/profileedit.dart
@@ -39,7 +39,6 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   TextFormField(
-                    // The validator receives the text that the user has entered.
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Please enter some text';
