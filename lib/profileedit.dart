d38eb55722c3b2a0d40dae9c420e730438fd9bbc
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-16 01:48:01 +0600

    Commit Thu 06/16/2022  1:48:01.20 17397

--- lib/profileedit.dart
+++ lib/profileedit.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:bjsandbarexam/globals.dart';
 
 class ProfileEditPage extends StatefulWidget {
-  final User userdata;
+  final Object userdata;
   ProfileEditPage(this.userdata);
   @override
   _ProfileEditPageState createState() => _ProfileEditPageState();
