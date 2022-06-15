23c04c4f77d2a0c4281e779e8e3bce4d3adaf9a7
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-16 01:44:10 +0600

    Commit Thu 06/16/2022  1:44:10.18 5548

--- lib/profileedit.dart
+++ lib/profileedit.dart
@@ -21,6 +21,7 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
     // ignore: todo
     // TODO: implement initState
     super.initState();
+    print(this.userdata);
   }
 
   @override
