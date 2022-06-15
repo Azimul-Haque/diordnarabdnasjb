b5e2c81cb3ab4c371a293b2a9f730e6f44f53b36
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-16 03:31:02 +0600

    Commit Thu 06/16/2022  3:31:02.15 30545

--- lib/profileedit.dart
+++ lib/profileedit.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors, prefer_const_constructors, unused_local_variable, avoid_print, unused_field
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:bjsandbarexam/globals.dart';
 
