bdc68830a9f603cee774f62b1246d868ba8c1f68
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-18 20:21:32 +0600

    Commit Sat 06/18/2022 20:21:32.15 28382

--- lib/home.dart
+++ lib/home.dart
@@ -82,7 +82,7 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      bottomNavigationBar: _bottomNavigationBar(),
+      bottomNavigationBar: bottomNavigationBar(),
     );
   }
 
