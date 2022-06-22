c38f38d57e5c4fe47c2170eb09da402fa22dc644
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-23 01:56:04 +0600

    Commit Thu 06/23/2022  1:56:04.24 12241

--- lib/globals.dart
+++ lib/globals.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 // import 'package:bjsandbarexam/home.dart';
 // import 'package:bjsandbarexam/profileedit.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
+import 'package:bjsandbarexam/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
