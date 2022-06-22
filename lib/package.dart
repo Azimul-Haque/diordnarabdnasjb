a189631e4df49fda5324057b460c0c241b1bd124
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-23 03:25:17 +0600

    Commit Thu 06/23/2022  3:25:17.13 27215

--- lib/package.dart
+++ lib/package.dart
@@ -26,19 +26,11 @@ class _PackagePageState extends State<PackagePage> {
   Widget build(BuildContext context) {
     String html = '''
     <html>
-    <head>
-      <title>Your Website Title</title> 
-    </head>
     <body>
-
-      <!-- Load Facebook SDK for JavaScript -->
       <div id="fb-root"></div>
       <script async defer src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2"></script>
-
-      <!-- Your embedded video player code -->
       <div class="fb-video" data-href="https://www.facebook.com/10msjobs/videos/1657928677897688" data-width="500" data-show-text="false">
       </div>
-
     </body>
     </html>
      ''';
