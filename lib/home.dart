84f221edaeb33eb05915ae56f85553aabb91c483
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-23 02:28:30 +0600

    Commit Thu 06/23/2022  2:28:30.19 2365

--- lib/home.dart
+++ lib/home.dart
@@ -143,9 +143,6 @@ class _HomeState extends State<Home> {
                   )
                 ],
               ),
-
-              // Right Tab bar icons
-
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
