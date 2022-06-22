e98dc4121f5cc7c5d6966521cae26589048293de
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-23 04:17:40 +0600

    Commit Thu 06/23/2022  4:17:40.22 30409

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -100,7 +100,7 @@ class _DashboardState extends State<Dashboard> {
               ),
               Divider(
                 color: Colors.grey,
-                height: 36,
+                height: 20,
               ),
               ElevatedButton(
                 style: ButtonStyle(
