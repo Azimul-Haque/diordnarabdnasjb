fac2f7fc602591581f50c4e6bf3cf8cf851463a0
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-23 04:08:41 +0600

    Commit Thu 06/23/2022  4:08:41.23 10022

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -53,7 +53,7 @@ class _DashboardState extends State<Dashboard> {
       // ),
       // drawer: _homeDrawer(),
       body: Container(
-        padding: ,
+        padding: const EdgeInsets.all(20),
         child: SingleChildScrollView(
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.spaceBetween,
