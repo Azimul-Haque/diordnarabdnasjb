c0a35f2c884a3446fabc7905c0e68007964b3642
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-25 02:02:19 +0600

    Commit Sat 06/25/2022  2:02:19.21 22117

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -244,7 +244,7 @@ class _DashboardState extends State<Dashboard> {
   Widget _scrollCard(String title, String subtitle, double screenwidth) {
     return SizedBox(
       height: 110,
-      width: screenwidth * .8,
+      width: screenwidth * .85,
       child: Card(
         child: Stack(
           children: <Widget>[
