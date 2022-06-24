eb1510baf7d69332e3354f9c129913b0115ea550
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-25 02:05:57 +0600

    Commit Sat 06/25/2022  2:05:57.26 17997

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -3,6 +3,7 @@
 import 'dart:convert';
 import 'dart:ffi';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -84,7 +85,7 @@ class _DashboardState extends State<Dashboard> {
                     child: TextButton(
                       child: Column(
                         children: const <Widget>[
-                          Icon(Icons.ac_unit),
+                          Icon(CupertinoIcons.add),
                           Text('বিজেএস'),
                         ],
                       ),
