97d6b5f3b19f4dece666467c7cd2fef5217a0d0e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-27 00:46:15 +0600

    Commit Mon 06/27/2022  0:46:15.23 30319

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -84,7 +84,10 @@ class _DashboardState extends State<Dashboard> {
               height: 20,
             ),
             // ignore: avoid_unnecessary_containers
-            Text("পরীক্ষাসমূহ"),
+            Text(
+              "পরীক্ষাসমূহ",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
             Container(
               padding: EdgeInsets.only(left: 10, right: 10),
               child: Row(
