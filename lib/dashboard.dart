152574dfaefb38a22534968d0c9c461909e23803
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-27 00:31:30 +0600

    Commit Mon 06/27/2022  0:31:30.19 12742

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -84,7 +84,7 @@ class _DashboardState extends State<Dashboard> {
               height: 20,
             ),
             Container(
-              padding: const EdgeInsets.all(10),
+              padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
               child: Row(
                 children: <Widget>[
                   Expanded(
