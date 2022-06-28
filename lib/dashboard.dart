bf4ff67520d1a45ff220250054239bf6005abda8
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 03:12:13 +0600

    Commit Wed 06/29/2022  3:12:13.22 24912

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -65,6 +65,7 @@ class _DashboardState extends State<Dashboard> {
               // width: double.infinity,
               child: ListView.builder(
                 padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+                physics: ClampingScrollPhysics(),
                 shrinkWrap: true,
                 scrollDirection: Axis.horizontal,
                 itemCount: courses.length,
