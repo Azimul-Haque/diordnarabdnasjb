51a6c0dee3ed384cc00a56ffe044091915a4fea6
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-03 01:35:07 +0600

    Commit Sun 07/03/2022  1:35:07.24 16852

--- lib/courses/singlecourse.dart
+++ lib/courses/singlecourse.dart
@@ -55,8 +55,8 @@ class _SingleCourseState extends State<SingleCourse> {
                       },
                     ))
                 : Center(
-                    ainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,/
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     child: SizedBox(
                       width: 50.0,
                       height: 50.0,
