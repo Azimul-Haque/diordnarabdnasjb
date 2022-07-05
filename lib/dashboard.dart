e401dfcb8a30edb87e96d334d0d2b3dcfa1f504d
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-06 01:24:22 +0600

    Commit Wed 07/06/2022  1:24:22.15 5453

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -179,8 +179,8 @@ class _DashboardState extends State<Dashboard> {
                         children: <Widget>[
                           Image.asset(
                             "assets/images/other-write.png",
-                            height: 60,
-                            width: 60,
+                            height: 50,
+                            width: 50,
                             alignment: Alignment.center,
                           ),
                           SizedBox(
