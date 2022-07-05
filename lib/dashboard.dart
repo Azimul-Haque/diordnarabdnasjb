53cb9d15796ad2f8d1029812296b043a4450cea2
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-06 01:41:36 +0600

    Commit Wed 07/06/2022  1:41:36.20 23739

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -350,7 +350,7 @@ class _DashboardState extends State<Dashboard> {
     );
   }
 
-  Widget _homeCard(String image, String title, Route routename) {
+  Widget _homeCard(String image, String title, routename) {
     return Card(
       child: Stack(
         children: <Widget>[
