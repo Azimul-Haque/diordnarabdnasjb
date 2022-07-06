d7dcef2f2b5319c64d0f5f8fc5859096d2bb7468
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-07 00:02:32 +0600

    Commit Thu 07/07/2022  0:02:32.22 24058

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -70,14 +70,17 @@ class _DashboardState extends State<Dashboard> {
             // WIDGET FOR PACKAGES AND HIDDEN API MESSGES
             // WIDGET FOR PACKAGES AND HIDDEN API MESSGES
             testpaymentconditional == false
-                ? Container(
-                    padding: EdgeInsets.all(10),
-                    color: Colors.green[100],
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        'দুঃখিত! কোন প্যাকেজ কেনা নেই! প্যাকেজ কিনুন।',
-                        textAlign: TextAlign.center,
+                ? InkWell(
+                    onTap: () {}, // Handle your callback
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      color: Colors.green[100],
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          'দুঃখিত! কোন প্যাকেজ কেনা নেই! প্যাকেজ কিনুন।',
+                          textAlign: TextAlign.center,
+                        ),
                       ),
                     ),
                   )
