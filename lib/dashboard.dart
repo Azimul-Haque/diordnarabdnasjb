514e93edc73b744fbf8f9ded89de10529b2df3cc
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-07 00:12:36 +0600

    Commit Thu 07/07/2022  0:12:36.15 13046

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -75,12 +75,11 @@ class _DashboardState extends State<Dashboard> {
                     color: Colors.green[100],
                     child: InkWell(
                       onTap: () {},
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          'দুঃখিত! কোন প্যাকেজ কেনা নেই! প্যাকেজ কিনুন।',
-                          textAlign: TextAlign.center,
-                        ),
+                      child: child: Ink(
+                        width: 200,
+                        height: 200,
+                        color: Colors.blue,
+                      ),
                       ),
                     ))
                 : Container(),
