6aa41a1121cde7d17121f1460a15b69fb527ea2e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-06 23:45:10 +0600

    Commit Wed 07/06/2022 23:45:10.16 32332

--- lib/globals.dart
+++ lib/globals.dart
@@ -105,7 +105,7 @@ appBarStyle() {
 
 commonAppBar(String appbarname, BuildContext context) {
   return AppBar(
-    elevation: 2,
+    elevation: 4,
     title: Text(appbarname),
     automaticallyImplyLeading: true,
     flexibleSpace: appBarStyle(),
