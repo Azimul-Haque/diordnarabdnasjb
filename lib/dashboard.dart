40972f37eb53f8248374361ef5e1b97c49efa9a6
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-06 23:53:28 +0600

    Commit Wed 07/06/2022 23:53:28.22 15686

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -74,7 +74,10 @@ class _DashboardState extends State<Dashboard> {
               color: Colors.green[100],
               child: SizedBox(
                 width: double.infinity,
-                child: Text('দুঃখিত! কোন প্যাকেজ কেনা নেই! প্যাকেজ কিনুন।'),
+                child: Text(
+                  'দুঃখিত! কোন প্যাকেজ কেনা নেই! প্যাকেজ কিনুন।',
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
             SizedBox(
