5e74d31f09cc44d6d3e7b3ab46d411a61819f4a5
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-06 23:40:44 +0600

    Commit Wed 07/06/2022 23:40:44.16 3545

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -69,7 +69,7 @@ class _DashboardState extends State<Dashboard> {
             // WIDGET FOR PACKAGES AND HIDDEN API MESSGES
             // WIDGET FOR PACKAGES AND HIDDEN API MESSGES
             // WIDGET FOR PACKAGES AND HIDDEN API MESSGES
-            Container(
+            Expanded(
               color: Colors.pink,
               child: Text('Test'),
             ),
