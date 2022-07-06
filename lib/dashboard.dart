36c0aea743c8ce5dd0ccd5154e006a7a0d5e0fce
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-07 00:08:00 +0600

    Commit Thu 07/07/2022  0:08:00.17 22613

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -71,6 +71,18 @@ class _DashboardState extends State<Dashboard> {
             // WIDGET FOR PACKAGES AND HIDDEN API MESSGES
             testpaymentconditional == false
                 ? TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.pressed))
+                            return Theme.of(context)
+                                .colorScheme
+                                .primary
+                                .withOpacity(0.5);
+                          return Colors.red; // Use the component's default.
+                        },
+                      ),
+                    ),
                     onPressed: () {}, // Handle your callback
                     child: Text(
                       'দুঃখিত! কোন প্যাকেজ কেনা নেই! প্যাকেজ কিনুন।',
