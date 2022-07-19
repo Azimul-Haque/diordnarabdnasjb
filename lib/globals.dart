e0e7599d25e97dbe8e1f73896845c02e26cc660b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 00:41:38 +0600

    Commit Wed 07/20/2022  0:41:38.23 18091

--- lib/globals.dart
+++ lib/globals.dart
@@ -13,7 +13,7 @@ import 'package:flutter/material.dart';
 
 // const String baseAPIURL = "https://bjsexam.com";
 // const String baseAPIURL = "http://192.168.122.39:8000";
-const String baseAPIURL = "http://192.168.0.102:8000";
+const String baseAPIURL = "http://192.168.150.39:8000";
 bool testpaymentconditional = true;
 bool noticemessage = false;
 // appBarStyle() {
