f2b5a438062b199bb730f84ed0a52ecae4ad4e96
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 01:01:12 +0600

    Commit Wed 07/20/2022  1:01:12.19 15115

--- lib/secondarypages/bjsmodeltests.dart
+++ lib/secondarypages/bjsmodeltests.dart
@@ -1,68 +1,91 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors, prefer_const_constructors, unused_local_variable, avoid_print, unused_field
 import 'dart:convert';
 
-import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
+import 'package:bjsandbarexam/bkash/testbkash.dart';
+import 'package:bjsandbarexam/courses/courseexam.dart';
 import 'package:bjsandbarexam/globals.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 
 class BJSModelTests extends StatefulWidget {
+  final String courseid;
+  BJSModelTests(this.courseid);
   @override
   _BJSModelTestsState createState() => _BJSModelTestsState();
 }
 
 class _BJSModelTestsState extends State<BJSModelTests> {
   final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
-
   late User userdata;
-  List courses = [];
+  List exams = [];
 
   @override
   void initState() {
     super.initState();
     userdata = FirebaseAuth.instance.currentUser!;
-    _getCourses();
+    // print(widget.courseid);
+    _getCoursesData();
   }
 
   @override
   Widget build(BuildContext context) {
+    double screenwidth = MediaQuery.of(context).size.width;
     return Scaffold(
       key: _scaffoldkey,
       appBar: AppBar(
-        title: Text('বিজেএস মডেল টেস্ট'),
+        title: Text('কোর্স'),
         flexibleSpace: appBarStyle(),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 10, bottom: 10, left: 15, right: 15),
-            child: Text("বিজেএস মডেল টেস্ট"),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            exams.isNotEmpty
+                ? ListView.builder(
+                    padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+                    physics: ClampingScrollPhysics(),
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
+                    itemCount: exams.length,
+                    itemBuilder: (context, index) {
+                      return _scrollCard(exams[index]["name"].toString(),
+                          exams[index]["exam_id"].toString(), screenwidth);
+                    },
+                  )
+                : Center(
+                    child: SizedBox(
+                      width: 50.0,
+                      height: 50.0,
+                      child: (CircularProgressIndicator()),
+                    ),
+                  ),
+          ],
+        ),
       ),
     );
   }
 
-  void _getCourses() async {
+  void _getCoursesData() async {
     try {
       String _softToken = "Rifat.Admin.2022";
       String serviceURL = baseAPIURL +
-          "/api/getcourses/" +
+          "/api/getcourses/exams/" +
           _softToken +
-          "/2"; // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT
+          "/" +
+          widget.courseid.toString();
       var response = await http.get(Uri.parse(serviceURL));
       if (response.statusCode == 200) {
         var body = json.decode(response.body);
         if (body["success"] == true) {
           // print("E PORJONTO");
-          var data = body["courses"];
+          var data = body["exams"];
           setState(() {
             for (var i in data) {
-              courses.add(i);
+              exams.add(i);
             }
           });
-          // print(courses);
+          // print(exams.length);
         } else {}
       } else {
         // print(response.body);
@@ -70,5 +93,80 @@ class _BJSModelTestsState extends State<BJSModelTests> {
     } catch (_) {
       // print(_);
     }
+  }
+
+  Widget _scrollCard(String title, String courseexameid, double screenwidth) {
+    return SizedBox(
+      height: 110,
+      width: screenwidth,
+      child: Card(
+        child: Stack(
+          children: <Widget>[
+            Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 9,
+                  width: double.infinity,
+                ),
+                SizedBox(
+                  height: 8,
+                  width: double.infinity,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Kalpurush',
+                    height: 1,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  courseexameid.toString(),
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 13,
+                    fontFamily: 'Kalpurush',
+                    height: 1.1,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            Positioned.fill(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    testpaymentconditional == true
+                        ? Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => CourseExam(courseexameid),
+                            ),
+                          )
+                        : Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => TestBkashPage(),
+                            ),
+                          );
+                  },
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ],
+        ),
+        margin: EdgeInsets.all(5),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        elevation: 2,
+        color: Colors.white,
+      ),
+    );
   }
 }
