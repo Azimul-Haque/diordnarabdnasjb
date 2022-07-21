3e02e6183f4c04f633af08b58e1de47dfb9ac370
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-22 01:38:57 +0600

    Commit Fri 07/22/2022  1:38:57.15 24731

--- lib/home.dart
+++ lib/home.dart
@@ -24,7 +24,7 @@ class _HomeState extends State<Home> {
   int _selectedItem = 0;
   List courses = [];
   final _pages = [
-    Dashboard(coursess),
+    Dashboard(courses),
     PackagePage(),
     ResultPage(),
     SettingsPage()
