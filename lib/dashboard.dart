98eb7bf7e561811a5aa7ea6873da8afe7b5d8b27
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-22 01:48:28 +0600

    Commit Fri 07/22/2022  1:48:28.17 17232

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -26,7 +26,6 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   late String uid;
   late User userdata;
-  List courses = [];
 
   @override
   void initState() {
