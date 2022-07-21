f64683d091e4b1c6e2ce9e98dd709302e642f1fc
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-22 02:01:41 +0600

    Commit Fri 07/22/2022  2:01:41.21 7715

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -450,7 +450,7 @@ class _DashboardState extends State<Dashboard> {
             // print(courses);
           } else {}
         } else {
-          print(response.body);
+          print(response);
         }
       } catch (_) {
         // print(_);
