2ce73d4e12631a9138539b29c02e535f321e279f
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-23 02:04:00 +0600

    Commit Sat 07/23/2022  2:04:00.21 396

--- lib/package.dart
+++ lib/package.dart
@@ -143,7 +143,7 @@ class _PackagePageState extends State<PackagePage> {
                   child: Row(
                     children: [
                       Text(
-                        bangla(package['price'].toString()),
+                        bangla(package['price']),
                         style: TextStyle(
                           color: Colors.black45,
                           fontSize: 13,
