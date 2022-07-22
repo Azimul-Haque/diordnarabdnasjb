83ebae3a7537f4da590c958cbd5c46f1ab7ea9de
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-23 00:53:15 +0600

    Commit Sat 07/23/2022  0:53:15.23 2698

--- lib/package.dart
+++ lib/package.dart
@@ -110,7 +110,7 @@ class _PackagePageState extends State<PackagePage> {
               // crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets.all(20.0),
+                  padding: const EdgeInsets.all(15.0),
                   child: Text(
                     title,
                     style: TextStyle(
