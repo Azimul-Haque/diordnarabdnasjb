ae8b15dda2fa3cafc808c622948b125b771c947a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-23 02:42:55 +0600

    Commit Sat 07/23/2022  2:42:55.24 17838

--- lib/secondarypages/freemodeltests.dart
+++ lib/secondarypages/freemodeltests.dart
@@ -78,32 +78,30 @@ class _FreeModelTestsState extends State<FreeModelTests> {
   }
 
   void _getCoursesData() async {
-    if (freemodeltests.isEmpty) {
-      try {
-        String _softToken = "Rifat.Admin.2022";
-        String serviceURL = baseAPIURL +
-            "/api/getothercourses/exams/" +
-            _softToken +
-            "/4"; // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT, 5 = QB
-        var response = await http.get(Uri.parse(serviceURL));
-        if (response.statusCode == 200) {
-          var body = json.decode(response.body);
-          if (body["success"] == true) {
-            // print("E PORJONTO");
-            var data = body["exams"];
-            setState(() {
-              for (var i in data) {
-                freemodeltests.add(i);
-              }
-            });
-            print(freemodeltests.length);
-          } else {}
-        } else {
-          // print(response.body);
-        }
-      } catch (_) {
-        // print(_);
+    try {
+      String _softToken = "Rifat.Admin.2022";
+      String serviceURL = baseAPIURL +
+          "/api/getothercourses/exams/" +
+          _softToken +
+          "/4"; // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT, 5 = QB
+      var response = await http.get(Uri.parse(serviceURL));
+      if (response.statusCode == 200) {
+        var body = json.decode(response.body);
+        if (body["success"] == true) {
+          // print("E PORJONTO");
+          var data = body["exams"];
+          setState(() {
+            for (var i in data) {
+              freemodeltests.add(i);
+            }
+          });
+          print(freemodeltests.length);
+        } else {}
+      } else {
+        // print(response.body);
       }
+    } catch (_) {
+      // print(_);
     }
   }
 
