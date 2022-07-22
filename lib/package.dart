2108d0027964950f11355e917beebe2fecb0cd19
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-23 01:39:11 +0600

    Commit Sat 07/23/2022  1:39:11.19 22637

--- lib/package.dart
+++ lib/package.dart
@@ -124,7 +124,7 @@ class _PackagePageState extends State<PackagePage> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(0.0),
+                  padding: const EdgeInsets.only(left: 15, top: 15, bottom: 5),
                   child: Text(
                     title,
                     style: TextStyle(
