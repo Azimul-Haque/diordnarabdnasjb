daa727b16b755a3c1db3de8d1f486a9c2aa093e7
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-23 01:55:50 +0600

    Commit Sat 07/23/2022  1:55:50.22 3093

--- lib/globals.dart
+++ lib/globals.dart
@@ -227,8 +227,6 @@ bangla(var str) {
   List en = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   List en = ['১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯', '০'];
   // var str = str_replace(en, bn, str);
-  List<String> coded = ["GM", "HOT", "YAH"]; //ABV list
-  List<String> decoded = ["Gmail", "Hotmail", "Yahoo"]; //corresponding list
   Map map = Map.fromIterables(en, en);
 
   String txt = "HOT was the best until GM took over"; //input text
