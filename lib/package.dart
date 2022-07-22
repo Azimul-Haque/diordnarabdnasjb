43af1ce5e7aaa2498e6baa3826d76bc55382de3b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-23 02:26:26 +0600

    Commit Sat 07/23/2022  2:26:26.25 14448

--- lib/package.dart
+++ lib/package.dart
@@ -196,7 +196,7 @@ class _PackagePageState extends State<PackagePage> {
         ),
         margin: package['suggested'] == 1
             ? EdgeInsets.only(top: 5, bottom: 5, left: 1, right: 1)
-            : EdgeInsets.all(5),
+            : EdgeInsets.all(10),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(10)),
         ),
