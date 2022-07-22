f3776eaefe3c2863a30e78aaa17049ca6fb31f1b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-23 02:12:50 +0600

    Commit Sat 07/23/2022  2:12:50.21 15971

--- lib/globals.dart
+++ lib/globals.dart
@@ -225,7 +225,7 @@ commonAppBar(String appbarname, BuildContext context) {
 
 String banglaNumber(String input) {
   const english = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const farsi = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+  const farsi = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯'];
 
   for (int i = 0; i < english.length; i++) {
     input = input.replaceAll(english[i], farsi[i]);
